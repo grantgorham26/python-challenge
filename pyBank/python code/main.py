78ce443bf57de6d28ed7c80f99a543b25f7a2930
Grant Gorham <grantegorham26@gmail.com>
2024-02-20 21:54:24 -1000

    finished code

--- pyBank/python code/main.py
+++ pyBank/python code/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 #create list to store data from csv file
 date = []
-total = []
+profit = []
 profit_change = []
 #set variables
 
@@ -25,12 +25,24 @@ with open(budget_csv,'r') as csvfile:
         #this gives the total number of months in the list
         num_months = len(date)
         #add total to a list and convert to an integer since its a string and i want a number
-        total.append(int(row[1]))
+        profit.append(int(row[1]))
         #this sums the losses and profits of the list 
         #https://www.geeksforgeeks.org/sum-function-python/ this is where i got code
-        total_profits= sum(total)
-        #change in profit is the specific month minus the previous month, ex: profit[1]-profit[0], or final profit minus the inital profit and then add that to a list and then take the average of that list
+        total_profits= sum(profit)
         
+        
+#change in profit/loss is the specific month minus the previous month, ex: profit[1]-profit[0], or final profit minus the inital profit and then add that to a list and then take the average of that list
+#used xpert leaning assistan to help find this out. i asked it how to loop through a list of and find the change between the numbers
+for i in range(1,len(profit)):
+    change = profit[i]- profit[i-1]
+    profit_change.append(change)
+    avgprofitchange = (sum(profit_change))/(len(profit_change))
+    greatest_increase_profit = max(profit_change)
+    index_greatest_increase= profit_change.index(max(profit_change))
+    greatest_decrease_profit = min(profit_change)
+    index_greatest_decrease= profit_change.index(min(profit_change))
+
+
 
 
         
@@ -44,4 +56,12 @@ print(num_months)
 #this sums the losses and profits of the list 
 print(total_profits)
 
+print(avgprofitchange)
 
+print(greatest_increase_profit)
+
+print(greatest_decrease_profit)
+
+print(date[index_greatest_decrease+1])
+
+print(date[index_greatest_increase+1])
