123a47b2c3c154568339aeca8ace3d9256df2b4a
Grant Gorham <grantegorham26@gmail.com>
2024-02-21 16:45:36 -1000

    added new more efficient code for pypoll

--- pyPoll/python code/main.py
+++ pyPoll/python code/main.py
@@ -36,48 +36,38 @@ with open(election_csv,'r') as csvfile:
             candidate_counter_dict[candidate_name] = candidate_counter_dict[candidate_name] + 1
         else:
             candidate_counter_dict[candidate_name] = 1
-        
-
-#obtain the vote count of each unique candidate         
-vote_count_charles = candidate_counter_dict[unique_candidate_list[0]]
-vote_count_diana = candidate_counter_dict[unique_candidate_list[1]]
-vote_count_raymon = candidate_counter_dict[unique_candidate_list[2]]
-#obtain the percentage of votes for each candidate
-percent_vote_charles = round((vote_count_charles/num_voters)*100,3)
-percent_vote_diana = round((vote_count_diana/num_voters)*100,3)
-percent_voters_raymon = round((vote_count_raymon/num_voters)*100,3)
-#obtain the max amount of votes from list of amount of votes from the counter dictionary 
+#obtain the max amount of votes from list of amount of votes from the counter dictionary            
 highest_vote_count = max(unique_candidate_list_votes)
 #obtain index of the candidate with highest votes
 index_of_winner = unique_candidate_list_votes.index(max(unique_candidate_list_votes))
 #obtain name of winner from list
 winning_candidate = unique_candidate_list[index_of_winner]
 
-
-
-#print results into terminal
+#need to comment more on the code
 print('Election Results')
 print('---------------------------------------')
 print(f'total votes: {num_voters}')
-print('---------------------------------------')
-print(f'{unique_candidate_list[0]}: {percent_vote_charles}% ({vote_count_charles})')
-print(f'{unique_candidate_list[1]}: {percent_vote_diana}% ({vote_count_diana})')
-print(f'{unique_candidate_list[2]}: {percent_voters_raymon}% ({vote_count_raymon})')
+print('---------------------------------------')        
+for i in range(len(unique_candidate_list)):
+    percent_of_votes = round((unique_candidate_list_votes[i]/num_voters)*100,3)
+    print(f'{unique_candidate_list[i]}: {percent_of_votes}% ({unique_candidate_list_votes[i]})')
 print('---------------------------------------')
 print(f'Winner: {winning_candidate}')
-print('---------------------------------------')
+print('---------------------------------------')   
 
 #write the results into a separate txt file 
 #used day 2 activity 10 and activtiy 12 to help write the code to write the files into a new txt file
-output_path = os.path.join('..','analysis','Results_analysis')
+output_path = os.path.join('..','analysis','Poll_Results_analysis.txt')
 with open(output_path,'w') as txtfile:
     txtfile.write('Election Results')
     txtfile.write('---------------------------------------')
     txtfile.write(f'total votes: {num_voters}')
+    txtfile.write('---------------------------------------')        
+    for i in range(len(unique_candidate_list)):
+        percent_of_votes = round((unique_candidate_list_votes[i]/num_voters)*100,3)
+    txtfile.write(f'{unique_candidate_list[i]}: {percent_of_votes}% ({unique_candidate_list_votes[i]})')
     txtfile.write('---------------------------------------')
-    txtfile.write(f'{unique_candidate_list[0]}: {percent_vote_charles}% ({vote_count_charles})')
-    txtfile.write(f'{unique_candidate_list[1]}: {percent_vote_diana}% ({vote_count_diana})')
-    txtfile.write(f'{unique_candidate_list[2]}: {percent_voters_raymon}% ({vote_count_raymon})')
+    txtfile.write(f'Winner: {winning_candidate}')
     txtfile.write('---------------------------------------')
-    txtfile.write(f'Winner: {unique_candidate_list[index_of_winner]}')
-    txtfile.write('---------------------------------------')
+        
+
